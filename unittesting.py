a56319499a0f5b4b7cfacf79c9fca65d5836246c
Matthew Gleich <matthewgleich@gmail.com>
2019-05-28 13:48:11 -0400

    Create unit testing for the current_date function

---
+++ unittesting.py
@@ -0,0 +1,18 @@
+import unittest
+import datetime
+import datetime_functions as DF
+
+
+class UnitTesting(unittest.TestCase):
+    """
+    Unit testing for this project
+    """
+
+    def test_current_date(self):
+        """
+        Will test the function current_date
+        :function location: ./datetime_functions.py
+        """
+        result = DF.current_date()
+        self.assertEqual(str(type(result)), "<class 'str'>")
+        self.assertEqual(len(result), 10)
