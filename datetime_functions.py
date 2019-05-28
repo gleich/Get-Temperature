2f0b5530086a4c123be27665107272896108bfaf
Matthew Gleich <matthewgleich@gmail.com>
2019-05-28 14:12:02 -0400

    Create current_time function

--- datetime_functions.py
+++ datetime_functions.py
@@ -13,3 +13,28 @@ def current_date():
 
 # Testing
 # print(current_date())
+
+
+def current_time():
+    """
+    Ge the current time of day
+    :return: string
+    """
+    tday = str(datetime.datetime.now())
+    elements = tday.split(" ")
+    time_selection = elements[1]
+    time_elements = time_selection.split(":")
+    hour = int(time_elements[0])
+    minute = int(time_elements[1])
+    second = float(time_elements[2])
+    if hour <= 12:
+        morn_night = "AM"
+    else:
+        morn_night = "PM"
+        hour -= 12
+    string = "{H}:{M}:{S} {TD}".format(H=str(hour),M=str(minute),S=str(second),TD=str(morn_night))
+    return string
+
+
+# Testing
+# print(current_time())
