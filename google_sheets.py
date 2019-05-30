6bb8f3b6332a3e3948606e4d6cb8dc4eb15d0d05
Matthew Gleich <matthewgleich@gmail.com>
2019-05-29 23:03:12 -0400

    Setup basic usage V1

--- google_sheets.py
+++ google_sheets.py
@@ -1,11 +1,19 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from oauth2client.service_account  import ServiceAccountCredentials
 
 
-scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
-creds =ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("gs_creds.json", scope)
 
 client = gspread.authorize(creds)
 
 sheet = client.open("TH_Program").sheet1
+
+data = sheet.get_all_records()
+
+row = sheet.row_values(3)
+
+insertRow = ["Hello", "5", "red", "blue"]
+
+sheet.insert_row(insertRow, 4)
