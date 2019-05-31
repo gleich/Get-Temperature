5ec2e4404435232c86c225274a81068f6fb3bd80
Matthew Gleich <matthewgleich@gmail.com>
2019-05-30 22:05:34 -0400

    Add update_stored_sheet function

--- google_sheets.py
+++ google_sheets.py
@@ -2,26 +2,36 @@ import gspread
 from oauth2client.service_account  import ServiceAccountCredentials
 
 
-def update_sheet(temp, humidity, date, time):
+def update_new_version_sheet(temp, temp_f, humidity, date, time, sheet):
     """
     Will update the data on the google sheet
     :param temp: temperature from the raspberry pi
+    :param temp_f: temperature from the raspberry pi in Fahrenheit
     :param humidity: humidity from the raspberry pi
     :param date: current date
     :param time: reading time
+    :param sheet: sheet that will be updated
     """
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
-             "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("gs_creds.json", scope)
-    client = gspread.authorize(creds)
-    sheet = client.open("TH_Program").sheet1
     sheet.update_cell(2, 1, temp)
-    sheet.update_cell(2, 2, humidity)
-    sheet.update_cell(2, 3, date)
-    sheet.update_cell(2, 4, time)
-    return None
-
+    sheet.update_cell(2, 2, temp_f)
+    sheet.update_cell(2, 3, humidity)
+    sheet.update_cell(2, 4, date)
+    sheet.update_cell(2, 5, time)
 
 
 # Testing:
 # update_sheet(23, 34, 2, 3)
+
+
+def update_stored_sheet(temp, temp_f, humidity, date, time, sheet):
+    """
+    Will update the data on the google sheet that is storing all the data
+    :param temp: temperature from the raspberry pi
+    :param temp_f: temperature from the raspberry pi in Fahrenheit
+    :param humidity: humidity from the raspberry pi
+    :param date: current date
+    :param time: reading time
+    :param sheet: sheet that will be updated
+    """
+    row = [temp, temp_f, humidity, date, time]
+    sheet.insert_row(row, 2)
