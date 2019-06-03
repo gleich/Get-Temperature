7c651c4108aad0b69525519d84fa7171971cd44f
Matthew Gleich <matthewgleich@gmail.com>
2019-06-03 18:39:22 -0400

    Create light function

--- rasp_pi_functions.py
+++ rasp_pi_functions.py
@@ -1,4 +1,6 @@
 import Adafruit_DHT
+import RPi.GPIO as GPIO
+import datetime_functions as DF
 
 
 def get_DHT11_output():
@@ -15,3 +17,24 @@ def get_DHT11_output():
 
 # Testing
 # print(get_DHT11_output())
+
+
+def light(pin):
+    """
+    Will light the blue light. This light is to show that program is running
+    :param pin: the GPIO pin number of the LED.
+    :param high: if the light should be high or low. Boolean
+    :return: None
+    """
+    if DF.day_time():
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.HIGH)
+        GPIO.cleanup()
+    else:
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(pin, GPIO.OUT)
+        p = GPIO.PWM(7,100)
+        p.start(0)
+        p.stop()
+        GPIO.cleanup()
