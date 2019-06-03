df49f887435a576de209b6e25699c93beeea744a
Matthew Gleich <matthewgleich@gmail.com>
2019-06-03 18:56:34 -0400

    Add dimming

--- rasp_pi_functions.py
+++ rasp_pi_functions.py
@@ -26,7 +26,13 @@ def light(pin):
     :param high: if the light should be high or low. Boolean
     :return: None
     """
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.HIGH)
-    GPIO.cleanup()
+    if DF.day_time():
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.HIGH)
+    else:
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(pin, GPIO.OUT)
+        p = GPIO.PWM(7, 100)
+        p.start(0)
+        p.stop()
